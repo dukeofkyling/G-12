ab6f56d9e0b8f354808e476441f8907bc76b3c0c
dukeofkyling <freshlawrencejunior@gmail.com>
2024-07-25 11:37:06 +0300

    heading

--- app/Imports/AnswersImport.php
+++ app/Imports/AnswersImport.php
@@ -6,7 +6,7 @@ use App\Models\Answer;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class AnswersImport implements ToModel
+class AnswersImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
